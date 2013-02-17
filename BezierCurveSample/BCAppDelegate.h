b8c2ee683e9b3ed31b3437ecca15e58854f4bfc7
oropon <momochitose@gmail.com>
2013-02-17 09:58:42 +0900

    Initial Commit

---
+++ BezierCurveSample/BCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BCAppDelegate.h
+//  BezierCurveSample
+//
+//  Created by Shogo on 2013/02/17.
+//  Copyright (c) 2013年 oropon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BCAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ BezierCurveSample/BCAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  BCAppDelegate.m
+//  BezierCurveSample
+//
+//  Created by Shogo on 2013/02/17.
+//  Copyright (c) 2013年 oropon. All rights reserved.
+//
+
+#import "BCAppDelegate.h"
+
+@implementation BCAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
